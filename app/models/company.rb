815fb3720ac4b0a6daab0ebcf2600e9b61c575cd
Kevin Guerrero <guerrk83z@gmail.com>
2022-01-18 11:32:32 -0500

    commit tree b1f41839d52a3461339922c97b46722f83154267

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,15 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
+
+    def give_freebie dev, item_name, value
+        dev1 = dev
+        Freebie.create(item_name: item_name, value: value, company_id: self.id, dev_id: dev1.id)
+    end
+
+    def self.oldest_company
+        Company.order(:founding_year).first
+    end
+
 
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,20 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    def recieved_one? item_name
+        self.freebies.each do |freebie|
+            if freebie.item_name == item_name
+                return true
+            end
+        end
+        false
+    end
+
+    def give_away dev, freebie
+        if self.id == freebie.dev_id.to_i
+            freebie.update(dev_id: dev.id)
+        end
+    end
 
 end
